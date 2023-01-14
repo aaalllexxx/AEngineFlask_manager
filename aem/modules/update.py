6cd72ac514d6ee47f9c123517c7ce6dfb166d3f2
aaalllexxx <a.aabdelnur@mail.ru>
2023-01-14 23:40:48 +0300

    fixed bugs

--- aem/modules/update.py
+++ aem/modules/update.py
@@ -4,5 +4,15 @@ import subprocess
 
 
 def main(base_dir, args):
-    os.chdir(f"C:/Users/{getpass.getuser()}/aem")
-    subprocess.call("git pull")
+    base = f"C:/Users/{getpass.getuser()}/aem"
+    if not os.path.exists(os.path.join(base, "temp")):
+        os.mkdir(os.path.join(base, "temp"))
+    os.chdir(os.path.join(base, "temp"))
+    temp = os.path.join(base, "temp")
+    aem = os.path.join(temp, "AEngineFlask_manager")
+    if not os.path.exists(aem):
+        subprocess.call("git clone https://github.com/aaalllexxx/AEngineFlask_manager/")
+    else:
+        os.chdir(aem)
+        subprocess.call("git pull")
+    os.system(os.path.join(aem, "init.bat"))
