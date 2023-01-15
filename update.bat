7c6e2ce5402a48aae065ee019eec65cc846196bd
aaalllexxx <a.aabdelnur@mail.ru>
2023-01-15 03:44:59 +0300

    fixed update.bat

--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 @echo off
-rd -r "C:/Users/%USERNAME%/aem"
+rmdir /s "C:/Users/%USERNAME%/aem"
 if not exist "C:/Users/%USERNAME%/aem" mkdir "C:/Users/%USERNAME%/aem"
 xcopy /s /i /Y /C "%cd%\aem\*.*"  "C:/Users/%USERNAME%/aem"
