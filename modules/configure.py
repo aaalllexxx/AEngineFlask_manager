d7058e9572f4a72a896e0ec77791d90b421f9c51
aaalllexxx <a.aabdelnur@mail.ru>
2023-01-17 02:57:38 +0300

    added modules

--- modules/configure.py
+++ modules/configure.py
@@ -1,7 +1,9 @@
 import os
 
-if not os.path.isdir("venv"):
-    os.system("python -m venv venv")
-    os.system("venv/Scripts/activate")
-    os.system("pip install -r ../req.txt")
-print("ready")
+
+def main(base_dir, args):
+    if not os.path.isdir("venv"):
+        os.system("python -m venv venv")
+        os.system("venv/Scripts/activate")
+        os.system("pip install -r ../req.txt")
+    print("ready")
--- modules/update.py
+++ modules/update.py
@@ -2,7 +2,6 @@ import getpass
 import os
 import shutil
 import subprocess
-import sys
 
 
 def copy_and_overwrite(from_path, to_path):
@@ -11,8 +10,8 @@ def copy_and_overwrite(from_path, to_path):
     shutil.copytree(from_path, to_path)
 
 
-def main(*_):
-    if not ("--full" in sys.argv):
+def main(_, args):
+    if not ("--full" in args):
         base = f"C:/Users/{getpass.getuser()}/aem"
         if not os.path.exists(os.path.join(base, "temp")):
             os.mkdir(os.path.join(base, "temp"))
