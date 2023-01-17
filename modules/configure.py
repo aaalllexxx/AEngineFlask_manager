ad974023034dc7138472d165a9b5096ff097a981
Абдельнур Алекс Фатех <a.aabdelnur@mail.ru>
2023-01-17 03:36:48 +0300

    added modules

--- modules/configure.py
+++ modules/configure.py
@@ -1,3 +1,4 @@
+import getpass
 import os
 
 
@@ -5,5 +6,5 @@ def main(*_):
     if not os.path.isdir("venv"):
         os.system("python -m venv venv")
         os.system("./venv/Scripts/activate")
-        os.system("pip install -r ../req.txt")
+        os.system(f"pip install -r C:/Users/{getpass.getuser()}/aem/req.txt")
     print("ready")
