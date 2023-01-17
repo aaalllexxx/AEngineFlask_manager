0ffe862e69dcbd82711fbdb469bb57429ea643b4
Абдельнур Алекс Фатех <a.aabdelnur@mail.ru>
2023-01-17 03:54:33 +0300

    added modules

--- modules/configure.py
+++ modules/configure.py
@@ -6,6 +6,5 @@ def main(*_):
     if not os.path.isdir("venv"):
         os.system("python -m venv venv")
     os.system("cd venv/Scripts/")
-    os.system("activate.bat")
     os.system(f"pip install -r C:/Users/{getpass.getuser()}/aem/req.txt")
     print("ready")
