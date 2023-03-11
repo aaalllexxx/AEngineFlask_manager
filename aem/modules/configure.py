f0a371e60342df9c6c7393ad4ca37601d99a15a6
alex_alex_good <allxgames@mail.ru>
2023-03-11 23:14:46 +0300

    test edit

--- aem/modules/configure.py
+++ aem/modules/configure.py
@@ -1,12 +1,10 @@
 import getpass
 import os
-import subprocess
 
 
 def main(*_):
     if not os.path.isdir("venv"):
         os.system("python -m venv venv")
     os.system("cd venv/Scripts/")
-    subprocess.call("./activate")
     os.system(f"pip install -r C:/Users/{getpass.getuser()}/aem/req.txt")
-    print("Ready. Type 'venv/Scripts/activate' to activate virtual environment")
+    print("ready")
